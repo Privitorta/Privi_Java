74dcbef516c3c7baf7183b0b4923ae13ccaf5933
Privitorta <125799472+Privitorta@users.noreply.github.com>
2025-07-26 14:48:43 +0200

    new Videoteca edition (copie film)

---
+++ Videoteca_v2/Cliente.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Cliente {
+    private int id;
+    private String nome;
+    private List<Film> filmNoleggiati;
+    
+    public Cliente(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+        this.filmNoleggiati = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void noleggiaFilm(Film film) {
+        filmNoleggiati.add(film);
+    }
+    public boolean restituisciFilm(int idFilm) {
+        return filmNoleggiati.removeIf(f -> f.getId() == idFilm);
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{id=" + id + ", nome='" + nome + "', filmNoleggiati=" + filmNoleggiati + "}";
+    }
+}
---
+++ Videoteca_v2/Film.java
@@ -0,0 +1,44 @@
+public class Film {
+    private int id;
+    private int copie;
+    private String titolo;
+    private String genere;
+
+    public Film(int id, int copie, String titolo, String genere) {
+        this.id = id;
+        this.copie = copie;
+        this.titolo = titolo;
+        this.genere = genere;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public String getGenere() {
+        return genere;
+    }
+
+    // gestione copie
+    public int getCopie() {
+        return copie;
+    }
+    public void incrementaCopie() {
+        copie++;
+    }
+    public void decrementaCopie() {
+        if (copie > 0) {
+            copie--;
+        } else {
+            System.out.println("Nessuna copia disponibile per il decremento");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Film{id=" + id + ", titolo='" + titolo + "', genere='" + genere + "', copie='" + copie + "'}";
+    }
+}
